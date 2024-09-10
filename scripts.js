8fa162b647a5e17c134f4be7f13f4304f7ad7232
Sarahpossfelt <95sarahhansen@gmail.com>
2024-09-10 14:36:40 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 function visaRecept(receptNamn) {
     alert("Här kommer mer information om " + receptNamn + " snart!");
 }
+document.getElementById('cta-button').addEventListener('click', function(event) {
+    var clickID = event.target.id;
+    var clickText = event.target.getAttribute('data-text');
+    console.log('Click ID:', clickID);
+    console.log('Click Text:', clickText);
+
+    // Här kan du lägga till Google Tag Manager-event om du vill
+});
